63c44dbc05c4b9a3b4648a274c4ecd02c2d0c03f
DyachenkoVadim1 <2052808@gmail.com>
2025-01-26 15:45:13 +0300

    Add files via upload

---
+++ ExportClass/Exporter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ExportClass
+{
+    public class Exporter
+    {
+        public void TxtExporter(string path, List<TaskExport> exportList)
+        {
+            TaskExport taskExport = new TaskExport();
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    foreach (TaskExport export in exportList)
+                    {
+                        string text = $"Наименование: {export.NameTask}\nОписание: {export.DescriptionTask}\nДедлайн: {export.Deadline}\nСтатус выполнения: {(export.NameStatus)}";
+                        writer.WriteLine(text);
+                    }
+                }
+            }
+            catch { }
+        }
+        public void jsonExporter(string path, List<TaskExport> tasks)
+        {
+            try
+            {
+                var options = new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                    Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+                };
+                string jsonString = JsonSerializer.Serialize(tasks, options);
+
+                using (StreamWriter writer = new StreamWriter(path, false, new UTF8Encoding(true)))
+                {
+                    writer.Write(jsonString);
+                }
+            }
+            catch { }
+        }
+    }
+
+    public class TaskExport
+    {
+        public string NameTask { get; set; }
+        public string DescriptionTask { get; set; }
+        public DateTime? Deadline { get; set; }
+        public string NameStatus { get; set; }
+    }
+}
